ec6e711e1d11ee27d49d8a4d9cb5ebb29205f5ae
Yaroslav Zvonok <aroslavzvonok876@gmail.com>
2024-11-14 23:20:22 +0200

    task form

---
+++ my-app/src/App.js
@@ -0,0 +1,114 @@
+import "./App.css";
+import { useState, useRef } from "react";
+
+const sendData = (formData) => {
+  console.log(formData);
+};
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [secondPassword, setSecondPassword] = useState("");
+  const [error, setError] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const submitButtonRef = useRef(null);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (password !== secondPassword) {
+      setError("пароли не совпадают");
+    } else {
+      setError("");
+    }
+    sendData({ email, password, secondPassword });
+    setSubmitted(true);
+    submitButtonRef.current.focus();
+  };
+
+  const onEmailChange = ({ target }) => {
+    setEmail(target.value);
+
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(target.value)) {
+      setError("");
+    } else {
+      setError("введите корректный email");
+    }
+  };
+
+  const onPasswordChange = ({ target }) => {
+    if (!submitted) {
+      setPassword(target.value);
+
+      if (target.value.length < 6) {
+        setError("пароль должен быть больше 6 символов");
+      } else if (secondPassword && target.value !== secondPassword) {
+        setError("пароли не совпадают");
+      } else {
+        setError("");
+      }
+    }
+  };
+
+  const onSecondPasswordChange = ({ target }) => {
+    if (!submitted) {
+      setSecondPassword(target.value);
+
+      if (target.value !== password) {
+        setError("пароли не совпадают");
+      } else {
+        setError("");
+      }
+    }
+  };
+
+  const reset = () => {
+    setEmail("");
+    setPassword("");
+    setSecondPassword("");
+    setError("");
+    setSubmitted(false);
+  };
+
+  const isButtonDisabled =
+    !email || !password || !secondPassword || !!error || submitted;
+
+  return (
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <p>Регистрация</p>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder="почта"
+          onChange={onEmailChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          value={password}
+          placeholder="пароль"
+          onChange={onPasswordChange}
+        />
+
+        <input
+          type="password"
+          name="secondPassword"
+          value={secondPassword}
+          placeholder="повтор пароля"
+          onChange={onSecondPasswordChange}
+        />
+        {error && <div className="error">{error}</div>}
+        <button type="submit" disabled={isButtonDisabled} ref={submitButtonRef}>
+          Зарегистрироваться
+        </button>
+        <button type="button" onClick={reset}>
+          сброс
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
